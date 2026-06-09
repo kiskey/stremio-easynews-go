ef9d063ac5946d3a1288ad930292c7f49395012d
kiskey <59226371+kiskey@users.noreply.github.com>
2026-06-09 15:38:38 -0700

    Update version.go

--- internal/shared/version.go
+++ internal/shared/version.go
@@ -1,8 +1,8 @@
 package shared
 
 // Version is set at build time via compiler ldflags.
-// Default value is "dev".
-var Version = "dev"
+// Bumped to v2.8.0 to support Dynamic TMDB Alternative Title Resolution.
+var Version = "2.8.0"
 
 // GetVersion returns the current version of the application.
 func GetVersion() string {
