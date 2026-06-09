5774f59b7b01d1d0bb9b15bc0c2594d6f434a006
kiskey <59226371+kiskey@users.noreply.github.com>
2026-06-09 15:57:37 -0700

    Update version.go

--- internal/shared/version.go
+++ internal/shared/version.go
@@ -1,8 +1,8 @@
 package shared
 
 // Version is set at build time via compiler ldflags.
-// Bumped to v2.8.0 to support Dynamic TMDB Alternative Title Resolution.
-var Version = "2.8.0"
+// Injected at compile time using -ldflags "-X github.com/kiskey/stremio-easynews-go/internal/shared.Version=2.8.2"
+var Version = "2.8.2"
 
 // GetVersion returns the current version of the application.
 func GetVersion() string {
